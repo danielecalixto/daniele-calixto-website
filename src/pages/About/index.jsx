a85de9a9a59c1461981ac096c406702483d52f7d
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 01:34:33 -0300

    feat: create initial template for About page and add TimeLine component

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom"
+import TimeLine from "../../components/TimeLine"
 
 const About = () => {
   return (
     <div>
       <p>About</p>
+      <TimeLine></TimeLine>
+      <div>
+        <p>More professional information:</p>
+        <button>RESUME</button>
+        <button>Linkedin</button>
+        <button>GitHub</button>
+        <button>Contact</button>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+      </div>
     </div>
   )
 }
