e0344ac71f1b3a58cff627ae9ebbf5d1fd6d769d
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 01:40:41 -0300

    feat: create TimeLine component with texts

---
+++ src/components/TimeLine/index.jsx
@@ -0,0 +1,34 @@
+const TimeLine = () => {
+  return (
+    <div>
+      <p>
+        I was born in 1994, in the country side of Minas Gerais state in Brazil.
+        At school, I was always from the Exact Sciences team. I always knew that
+        I want to be an engineer, but at that moment it was very difficult to
+        decide which engineering.
+      </p>
+      <h3>College</h3>
+      <p>
+        In 2015, I started the bachelor in Civil Engineering in the Brazilian
+        Federal Center of Technology (Cefet-MG). I tried an internship in
+        construction but definitely It wasn't for me. Still in college, I
+        decided to try other areas by practicing.
+      </p>
+      <h3>Working</h3>
+      <p>
+        I had an opportunity in an award products factory and that's where I
+        started my career in finances and process automation. After 2 years
+        there, I decided to look for new challenges and I changed to a
+        technology startup where I continued working with finance but I had more
+        contact with coding and technology.
+      </p>
+      <h3>Development</h3>
+      <p>
+        In 2021, I decided for fact to stop working and immerse in studying
+        technology for one year.
+      </p>
+    </div>
+  )
+}
+
+export default TimeLine
