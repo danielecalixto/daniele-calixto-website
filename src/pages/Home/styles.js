eb726527f26bc3037925d4872d5970091eedfedb
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 13:43:25 -0300

    style: create Image container and add styles to container

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,28 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  p {
-    font-family: "Courier New", Courier, monospace;
+  margin: 5px 10vw 5px 10vw;
+  width: 80vw;
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    color: #443235;
+  }
+
+  h3 {
+    margin-top: 20px;
+  }
+  .contact {
+    margin-top: 20px;
+    margin-bottom: 30px;
+  }
+`
+
+export const Images = styled.div`
+  img {
+    margin: 15px 10px 15px 0px;
+    width: 35px;
+    height: 35px;
   }
 `
