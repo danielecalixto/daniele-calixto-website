932e05fba09be18c40d0e61b3a28f451c659dc86
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 11:53:26 -0300

    fix: remove SideBar import

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 import Header from "../../components/Header"
-import SideBar from "../../components/SideBar"
 import { Container } from "./styles"
 
 const Home = () => {
