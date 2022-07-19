8041f6e291847176292d109602fa19922901b32f
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 01:39:25 -0300

    feat: create NavBar linking to all the app pages

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import { Container } from "./styles"
+
+const NavBar = () => {
+  return (
+    <Container>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About Me</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <hr></hr>
+    </Container>
+  )
+}
+
+export default NavBar
