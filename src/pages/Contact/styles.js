08ea333b444943981bea0b0dd95950f9bc621ebf
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 18:02:04 -0300

    style: add tag a style of Contact page

--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,11 +1,35 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  margin: 5px 10vw 0px 10vw;
+  margin: 5px 10vw 90vw 10vw;
   width: 80vw;
+  p {
+    display: flex;
+  }
+  img {
+    width: 25px;
+    margin-right: 10px;
+  }
 
+  a {
+    margin-bottom: 8px;
+    display: flex;
+    align-items: center;
+    font-family: "Courier New", Courier, monospace;
+
+    font-size: 12px;
+    color: #443235;
+  }
   @media (min-width: 650px) {
     width: 84vw;
-    margin: 5vw 8vw 0vw 8vw;
+    margin: 5vw 8vw 30vw 8vw;
+    img {
+      width: 40px;
+      height: 40px;
+      margin-right: 10px;
+    }
+    a {
+      font-size: 25px;
+    }
   }
 `
