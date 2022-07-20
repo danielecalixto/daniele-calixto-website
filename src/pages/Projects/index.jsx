e83ca520b203a56e35161bab97f980cfecce60bd
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 14:24:28 -0300

    feat: include links to Back to Home and Contact in Projects page and remove unused Link component

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom"
-
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import BackToHome from "../../components/BackToHome"
 
 import { Container } from "./styles"
 
@@ -12,6 +11,7 @@ const Projects = () => {
       <Container>
         <h2>Projects</h2>
       </Container>
+      <BackToHome></BackToHome>
       <Footer></Footer>
     </div>
   )
