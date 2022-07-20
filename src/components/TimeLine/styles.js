ea8a487daff4a8e6d84a02a2b05d2c14bc0ea786
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 14:36:38 -0300

    style: create media query for TimeLine component

--- src/components/TimeLine/styles.js
+++ src/components/TimeLine/styles.js
@@ -5,4 +5,28 @@ export const Container = styled.div`
     border-left: 2px solid black;
     padding-left: 10px;
   }
+  img {
+    width: 40px;
+    margin-right: 10px;
+  }
+  h3 {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  @media (min-width: 650px) {
+    p {
+      margin-top: 20px;
+      margin-bottom: 20px;
+      padding-left: 20px;
+    }
+    img {
+      width: 50px;
+      margin-right: 20px;
+    }
+    h3 {
+      margin-top: 40px;
+    }
+  }
 `
