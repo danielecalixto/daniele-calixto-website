00e88ae5085df6edd8f6ec070fec545dfbeff8e3
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 14:29:19 -0300

    style: include media query for desktop

--- src/pages/Articles/styles.js
+++ src/pages/Articles/styles.js
@@ -3,4 +3,9 @@ import styled from "styled-components"
 export const Container = styled.div`
   margin: 5px 10vw 0px 10vw;
   width: 80vw;
+
+  @media (min-width: 650px) {
+    width: 84vw;
+    margin: 5vw 8vw 0vw 8vw;
+  }
 `
