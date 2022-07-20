c5b583132660ea4b432973ab4d8d73ba5dce7bb3
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 22:45:59 -0300

    feat: include Footer component and Container styled component to Contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom"
+
 import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+
+import { Container } from "./styles"
 
 const Contact = () => {
   return (
     <div>
       <Header></Header>
-      <h2>Contact</h2>
+      <Container>
+        <h2>Contact</h2>
+      </Container>
+      <Footer></Footer>
     </div>
   )
 }
