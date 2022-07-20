ebc112370925d13499447153be2fb979dc61fa24
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 22:39:43 -0300

    style: add Container styles to Footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  margin-top: 40px;
+  margin-bottom: 20px;
+  p {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 10px;
+    text-align: center;
+  }
+`
