7db9a4472e5500a5fc74715334ff8ea7dafe9bba
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 22:46:33 -0300

    feat: include Footer component and Container styled component to Projects page

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom"
+
 import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+
+import { Container } from "./styles"
 
 const Projects = () => {
   return (
     <div>
       <Header></Header>
-      <h2>Projects</h2>
+      <Container>
+        <h2>Projects</h2>
+      </Container>
+      <Footer></Footer>
     </div>
   )
 }
