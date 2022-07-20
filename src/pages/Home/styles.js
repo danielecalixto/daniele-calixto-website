0157ccc7aded9204de0b9137eda8573442f4e9e9
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 22:50:47 -0300

    style: include desktop media query to Home page

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  margin: 5px 10vw 5px 10vw;
+  margin: 5px 10vw 0px 10vw;
   width: 80vw;
 
   a {
@@ -17,12 +17,30 @@ export const Container = styled.div`
     margin-top: 20px;
     margin-bottom: 30px;
   }
+
+  @media (min-width: 450px) {
+    margin: 5vw 5vw 0vw 5vw;
+    h3 {
+      margin-top: 5vw;
+    }
+    .contact {
+      margin-top: 2vw;
+      margin-bottom: 3vw;
+    }
+  }
 `
 
 export const Images = styled.div`
   img {
     margin: 15px 10px 15px 0px;
-    width: 35px;
-    height: 35px;
+    width: 40px;
+    height: 40px;
+  }
+  @media (min-width: 450px) {
+    img {
+      margin: 15px 25px 15px 0px;
+      width: 65px;
+      height: 65px;
+    }
   }
 `
