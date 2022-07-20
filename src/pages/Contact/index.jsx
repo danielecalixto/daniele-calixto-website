a468d03318ae5af0414b4905daffa9a617c28abf
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 18:01:13 -0300

    feat: add email, githun and linkeidn to contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -9,6 +9,29 @@ const Contact = () => {
       <Header></Header>
       <Container>
         <h2>Contact</h2>
+
+        <p>
+          <img
+            alt="Email"
+            src="https://img.icons8.com/ios/60/000000/send-mass-email.png"
+          />
+          danielecalixtob@gmail.com
+        </p>
+        <a href="https://www.linkedin.com/in/daniele-calixto-barros/">
+          <img
+            alt="Linkedin"
+            src="https://img.icons8.com/ios/50/000000/linkedin.png"
+          />
+          daniele-calixto-barros
+        </a>
+        <a href="https://github.com/danielecalixto">
+          <img
+            className="git"
+            alt="GitHub"
+            src="https://img.icons8.com/material-outlined/60/000000/github.png"
+          />
+          danielecalixto
+        </a>
       </Container>
       <Footer></Footer>
     </div>
