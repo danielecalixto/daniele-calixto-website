18c9895135a99ca7e064cf080708fd085dbf1f4c
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 14:25:06 -0300

    feat: include links to Back to Home and Contact in Articles page and remove unused Link component

--- src/pages/Articles/index.jsx
+++ src/pages/Articles/index.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom"
-
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import BackToHome from "../../components/BackToHome"
 
 import { Container } from "./styles"
 
@@ -13,6 +12,7 @@ const Articles = () => {
         <h2>Articles</h2>
         <p>Soon...</p>
       </Container>
+      <BackToHome></BackToHome>
       <Footer></Footer>
     </div>
   )
