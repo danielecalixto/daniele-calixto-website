dfc5b87c0f1586f8fa1bd8760254e54753f55bf6
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 22:39:16 -0300

    style: add Container styled component to Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,11 @@
+import { Container } from "./styles"
+
 const Footer = () => {
-  return <p>Made by Daniele Calixto</p>
+  return (
+    <Container>
+      <p>Made by Daniele Calixto Barros 2022</p>
+    </Container>
+  )
 }
 
 export default Footer
