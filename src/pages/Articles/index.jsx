99797dc3ba9534dc2174f61b21c4526868e064eb
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 22:45:35 -0300

    feat: include Footer component and Container styled component to Articles page

---
+++ src/pages/Articles/index.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+
+import { Container } from "./styles"
+
+const Articles = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <h2>Articles</h2>
+        <p>Soon...</p>
+      </Container>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default Articles
