e2442545b7f1b1045681c4000191e92bf67db847
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 14:37:21 -0300

    style: include icons for TimeLine component

--- src/components/TimeLine/index.jsx
+++ src/components/TimeLine/index.jsx
@@ -9,14 +9,26 @@ const TimeLine = () => {
         I want to be an engineer, but at that moment it was very difficult to
         decide which engineering.
       </p>
-      <h3>College</h3>
+      <h3>
+        <img
+          alt="College"
+          src="https://img.icons8.com/material/24/000000/student-female--v1.png"
+        />
+        College
+      </h3>
       <p>
         In 2015, I started the bachelor in Civil Engineering in the Brazilian
         Federal Center of Technology (Cefet-MG). I tried an internship in
         construction but definitely It wasn't for me. Still in college, I
         decided to try other areas by practicing.
       </p>
-      <h3>Working</h3>
+      <h3>
+        <img
+          alt="Finance"
+          src="https://img.icons8.com/external-sbts2018-mixed-sbts2018/58/000000/external-finance-economy1-sbts2018-mixed-sbts2018-1.png"
+        />
+        Working
+      </h3>
       <p>
         I had an opportunity in an award products factory and that's where I
         started my career in finances and process automation. After 2 years
@@ -24,7 +36,13 @@ const TimeLine = () => {
         technology startup where I continued working with finance but I had more
         contact with coding and technology.
       </p>
-      <h3>Development</h3>
+      <h3>
+        <img
+          alt="Development"
+          src="https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/64/000000/external-development-advertising-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png"
+        />
+        Development
+      </h3>
       <p>
         In 2021, I decided for fact to stop working and immerse in studying
         technology for one year.
