7c3b17dbf4d944fd24b664dd127a6714b7a04c4c
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 14:34:53 -0300

    feat: include links to Back to Home and Contact in About page, remove unused Link component and add links to Resume, Linkedin and GitHub

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom"
-
 import TimeLine from "../../components/TimeLine"
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import BackToHome from "../../components/BackToHome"
 
 import { Container } from "./styles"
 
@@ -14,16 +13,29 @@ const About = () => {
         <h2>About Me</h2>
         <TimeLine></TimeLine>
         <div>
-          <p>More professional information:</p>
-          <button>RESUME</button>
-          <button>Linkedin</button>
-          <button>GitHub</button>
-          <button>Contact</button>
-          <button>
-            <Link to="/">Home</Link>
-          </button>
+          <p>
+            More professional information?
+            <a href="https://www.linkedin.com/in/daniele-calixto-barros/">
+              My Resume.
+            </a>
+          </p>
+          <a href="https://www.linkedin.com/in/daniele-calixto-barros/">
+            <img
+              className="contact"
+              alt="Linkedin"
+              src="https://img.icons8.com/glyph-neue/60/000000/linkedin-circled.png"
+            />
+          </a>
+          <a href="https://github.com/danielecalixto">
+            <img
+              className="contact"
+              alt="GitHub"
+              src="https://img.icons8.com/fluency/60/000000/github.png"
+            />
+          </a>
         </div>
       </Container>
+      <BackToHome></BackToHome>
       <Footer></Footer>
     </div>
   )
