15217fdd7969df17ce3b7524ba8ed1f43ec70994
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-20 18:00:32 -0300

    style: increase bottom margin of Articles page

--- src/pages/Articles/styles.js
+++ src/pages/Articles/styles.js
@@ -1,11 +1,11 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  margin: 5px 10vw 0px 10vw;
+  margin: 5px 10vw 50vh 10vw;
   width: 80vw;
 
   @media (min-width: 650px) {
     width: 84vw;
-    margin: 5vw 8vw 0vw 8vw;
+    margin: 5vw 8vw 50vh 8vw;
   }
 `
