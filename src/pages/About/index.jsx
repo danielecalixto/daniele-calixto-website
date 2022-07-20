6f4a34f95a613d8febca817142ef31c71ee5675c
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 22:44:30 -0300

    feat: include Footer component and Container styled component to About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,23 +1,30 @@
 import { Link } from "react-router-dom"
+
 import TimeLine from "../../components/TimeLine"
 import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+
+import { Container } from "./styles"
 
 const About = () => {
   return (
     <div>
       <Header></Header>
-      <h2>About</h2>
-      <TimeLine></TimeLine>
-      <div>
-        <p>More professional information:</p>
-        <button>RESUME</button>
-        <button>Linkedin</button>
-        <button>GitHub</button>
-        <button>Contact</button>
-        <button>
-          <Link to="/">Home</Link>
-        </button>
-      </div>
+      <Container>
+        <h2>About Me</h2>
+        <TimeLine></TimeLine>
+        <div>
+          <p>More professional information:</p>
+          <button>RESUME</button>
+          <button>Linkedin</button>
+          <button>GitHub</button>
+          <button>Contact</button>
+          <button>
+            <Link to="/">Home</Link>
+          </button>
+        </div>
+      </Container>
+      <Footer></Footer>
     </div>
   )
 }
